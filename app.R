06b65b881de07e5f62211ae3a885030ba5cbd17a
Payton Redemer <paytonredemer@gmail.com>
2022-07-22 09:18:36 -0500

    added comments

--- app.R
+++ app.R
@@ -57,7 +57,7 @@ ui <- fluidPage(
       checkboxGroupInput(inputId = "subtype_options",
                          label = NULL,
                          choices = NULL),
-      # Dropdown specifically for mutations 
+      # Dropdown specifically for mutations
       selectInput(inputId = "mutation_status",
                   label = "Mutation Status",
                   choices = mutation_specific_genes$V1)
@@ -70,6 +70,7 @@ ui <- fluidPage(
 
 server <- function(input, output,session) {
   # Handle event when user selects dataset
+  # Changes gene list and available types of plots based on the dataset
   observeEvent(input$type, {
     itemPlot <<- "Protein"
     if(input$type == "TMT") {
@@ -106,8 +107,9 @@ server <- function(input, output,session) {
     updateSelectizeInput(session, 'subtype', choices = subtype, server = TRUE)
     if(input$type != "Protein vs mRNAs") {shinyjs::show(id = "subtype")}
   })
-  
+
   # Handle event when user selects subtype
+  # Toggles several ui elements based on the dataset and subtype
   observeEvent(input$subtype, {
     # Phosphosite doesn't allow you to toggle clinical options
     if(input$type == "Phosphosite") {
@@ -119,7 +121,7 @@ server <- function(input, output,session) {
     shinyjs::show(id = "gene")
     shinyjs::hide(id = "genes")
     shinyjs::hide(id = "mutation_status")
-    
+
     if(input$subtype == "Multiplot") {
       shinyjs::show(id = "genes")
       shinyjs::hide(id = "subtype_options")
@@ -171,26 +173,27 @@ server <- function(input, output,session) {
     }
   })
 
+  # Handles output for plot
   output$plot <- renderPlotly({
     plotReady <- FALSE
     # Multiplot
     if(input$subtype == "Multiplot" && length(input$genes) > 0) {
       query <- geneQuery(genes = input$genes,type = input$type)
       tcga <- dbGetQuery(database,query)
-      
+
       query <- clinicalQuery(factors="*", table=paste(input$type,"_Clinical",sep=""))
       clinical <- dbGetQuery(database,query)
-      
+
       clinical <- merge(tcga, clinical, by="UPN")
-      
+
       g <- ggplot(clinical,aes(fill=Gene, y=Value, x=Name, text = paste0("UPN ID: ",UPN,"<br />TCGA Sample ID: ", TCGA_ID))) + geom_bar(position="dodge", stat="identity") + theme_bw() +
         theme(text=element_text(size=12, family="avenir", face="bold"), axis.text=element_text(size=10, family="avenir", face="bold"),
               axis.title=element_text(size=12, family="avenir", face="bold"),
               axis.text.x = element_text(angle = 90, hjust = 1)) +
         ggtitle("Multiple protein view") +
         ylab("Log2 Expression") + xlab("")
-      
-      
+
+
       plotReady <- TRUE
     }
     # Subtype
@@ -207,7 +210,7 @@ server <- function(input, output,session) {
                              subtypes=input$subtype_options,
                              type="FAB")
       clinical <- dbGetQuery(database,query)
-      
+
       clinical <- merge(clinical,tcga, by="UPN")
       facet <- NULL
       if(input$type == "Phosphosite") {
@@ -222,7 +225,7 @@ server <- function(input, output,session) {
               axis.text=element_text(size=12, family="avenir", face="bold"),
               axis.text.x = element_text(angle = 45, hjust = 1)) +
         ylab("Log2 Expression") + xlab("")
-      
+
       g <- g + facet
       plotReady <- TRUE
     }
@@ -241,20 +244,20 @@ server <- function(input, output,session) {
                              type="Cyto_Risk")
       clinical <- dbGetQuery(database,query)
       clinical <- merge(clinical,tcga, by="UPN")
-      
+
       facet <- NULL
       if(input$type == "Phosphosite") {
         setnames(clinical, "Gene", "Phosphosite")
         facet <- facet_wrap(~ Phosphosite)
       }
       clinical$Gene <- input$gene
-      
+
       g <- ggplot(clinical,aes(Cyto_Risk, Value, text = paste0("UPN ID: ",Name,"<br />TCGA Sample ID: ", TCGA_ID))) + geom_quasirandom(size = 0.8) + theme_bw() +
         ggtitle(paste0("Log2 Expression for ",input$gene)) +
         theme(text=element_text(size=12, family="avenir", face="bold"), axis.text=element_text(size=12, family="avenir", face="bold"),
               axis.text.x = element_text(angle = 45, hjust = 1)) +
         ylab("Log2 Expression") + xlab("")
-      
+
       g <- g + facet
       plotReady <- TRUE
     }
@@ -273,20 +276,20 @@ server <- function(input, output,session) {
                              type="Fusion")
       clinical <- dbGetQuery(database,query)
       clinical <- merge(clinical,tcga, by="UPN")
-      
+
       facet <- NULL
       if(input$type == "Phosphosite") {
         setnames(clinical, "Gene", "Phosphosite")
         facet <- facet_wrap(~ Phosphosite)
       }
-      
+
       clinical$Gene <- input$gene
-      
+
       g <- ggplot(clinical,aes(Fusion, Value, text = paste0("UPN ID: ",Name,"<br />TCGA Sample ID: ", TCGA_ID))) + geom_quasirandom(size = 0.8) + theme_bw() +
         ggtitle(paste0("Log2 Expression for ",input$gene)) +
         theme(text=element_text(size=12, family="avenir", face="bold"),axis.text=element_text(size=12, family="avenir", face="bold"), axis.text.x = element_text(angle = 45)) +
         ylab("Log2 Expression") + xlab("")
-      
+
       g <- g + facet
       plotReady <- TRUE
     }
@@ -295,36 +298,36 @@ server <- function(input, output,session) {
       query <- geneQuery(factors = c("UPN","Value"),genes = input$gene, type = input$type)
       tcga <- dbGetQuery(database,query)
       tcga$Group <- "WT"
-      
+
       upns_with_mut <- as.character(mutation_table$UPN[which(mutation_table$Gene %in% input$mutation_status)])
-      
+
       query <- clinicalQuery(factors = "*",
                              table = paste0(input$type,"_Clinical"))
       all_clinical <- dbGetQuery(database,query)
-      
+
       upns_with_health <- as.character(all_clinical$UPN[which(all_clinical$TCGA_Name %like% "Healthy")])
-      
+
       tcga$Group[tcga$UPN %in% upns_with_mut] <- input$mutation_status
       tcga$Group[tcga$UPN %in% upns_with_health] <- all_clinical$FAB[tcga$UPN %in% upns_with_health]
-      
+
       tcga$Mutation <- paste(input$mutation_status,"WT")
       tcga$Mutation[tcga$UPN %in% upns_with_mut] <- mutation_table$Mutation[tcga$UPN %in% upns_with_mut]
       tcga$Mutation[tcga$UPN %in% upns_with_health] <- all_clinical$FAB[tcga$UPN %in% upns_with_health]
       tcga$Gene <- input$gene
-      
+
       clinical <- merge(tcga,all_clinical[,c("UPN","Name","TCGA_ID")], by = "UPN")
       clinical <- clinical[,c(1,6,7,3,4,2,5)]
       #TODO ADD Phosphosite
-      
+
       factorLevels <- unique(clinical$Group)
       factorLevels <- factorLevels[! factorLevels %in% mutationLevels]
       factorLevels <- append(factorLevels,mutationLevels)
       clinical$Group <- factor(clinical$Group,levels=factorLevels)
-      
-      g <- ggplot(clinical,aes(Group, Value, text = paste0("UPN ID: ",Name,"<br />Mutation type: ", Mutation,"<br />TCGA Sample ID: ", TCGA_ID))) + 
-        geom_quasirandom(size = 0.8) + 
+
+      g <- ggplot(clinical,aes(Group, Value, text = paste0("UPN ID: ",Name,"<br />Mutation type: ", Mutation,"<br />TCGA Sample ID: ", TCGA_ID))) +
+        geom_quasirandom(size = 0.8) +
         theme_bw() +
-        ggtitle(paste0("Log2 Expression for ",input$gene," with ",input$mutation_status, ": WT|MT")) + 
+        ggtitle(paste0("Log2 Expression for ",input$gene," with ",input$mutation_status, ": WT|MT")) +
         theme(text=element_text(size=12, family="avenir", face="bold"), axis.text=element_text(size=12, family="avenir", face="bold"),axis.text.x = element_text(angle = 45, hjust = 1)) +
         ylab("Log2 Expression") + xlab("")
       plotReady <- TRUE
@@ -334,15 +337,15 @@ server <- function(input, output,session) {
       query <- geneQuery(genes = input$genes,type = input$type)
       tcga <- dbGetQuery(database,query)
       print(input$genes)
-      
+
     }
-    
+
     # Plotting
     if(plotReady) {
       ggplotly(g, tooltip="text")
     }
-    
-  }) 
+
+  })
 }
 
 shinyApp(ui = ui, server = server)
