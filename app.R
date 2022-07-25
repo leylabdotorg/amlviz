c8b9ffb3fea15c7078959436ac0cbeea2477567a
Payton Redemer <paytonredemer@gmail.com>
2022-07-25 11:10:57 -0500

    improved formatting

--- app.R
+++ app.R
@@ -25,48 +25,70 @@ mRNA_choices <- unique(dbGetQuery(database, "SELECT Gene FROM Genes WHERE Type='
 # local files
 mutation_specific_genes <- read.table(file = "data/mutation_specific_genes.txt", sep="\t", skipNul=T, encoding="UTF-8", quote = "")
 mutation_table <- read.table(file = "data/mutations.txt", header=T, sep="\t", skipNul=T, encoding="UTF-8", quote = "")
-genenames_corr = read.table("data/correlation_genes.txt", stringsAsFactors=F, header=F, sep = "\t", skipNul=T, encoding="UTF-8", quote = "")
+genenames_corr <- read.table("data/correlation_genes.txt", stringsAsFactors=F, header=F, sep = "\t", skipNul=T, encoding="UTF-8", quote = "")
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       # Dropdown to select dataset
-      selectInput(inputId = "type",
-                  label = "Select a dataset",
-                  choices = c("TMT","LFQ","Phosphosite","mRNA","Protein vs mRNAs")),
-      conditionalPanel(condition = "!(input.type == 'Protein vs mRNAs')",
-                       # Dropdown to select which plot you want based on dataset
-                       selectizeInput(inputId = "subtype",
-                                      label = "Select an option",
-                                      choices = NULL,
-                                      multiple = FALSE)),
-      conditionalPanel(condition = "input.subtype == 'Multiplot' || input.type == 'Protein vs mRNAs'",
-                       # Dropdown to select genes
-                       selectizeInput(inputId = "genes",
-                                      label = NULL,
-                                      choices = NULL,
-                                      multiple = TRUE,
-                                      selected = NULL)),
-      conditionalPanel(condition = "!(input.subtype == 'Multiplot' || input.type == 'Protein vs mRNAs')",
-                       # Dropdown to select gene
-                       selectizeInput(inputId = "gene",
-                                      label = NULL,
-                                      choices = NULL,
-                                      multiple = FALSE,
-                                      selected = NULL)),
-      conditionalPanel(condition = "!(input.subtype == 'Multiplot' || input.subtype == 'Mutations' || input.type == 'Phosphosite' ||input.type == 'Protein vs mRNAs')",
-                       # Checkbox to select subtype options
-                       checkboxGroupInput(inputId = "subtype_options",
-                                          label = NULL,
-                                          choices = NULL)),
-      conditionalPanel(condition = "input.subtype == 'Mutations'",
-                       # Dropdown specifically for mutations
-                       selectInput(inputId = "mutation_status",
-                                   label = "Mutation Status",
-                                   choices = mutation_specific_genes$V1))
+      selectInput(
+        inputId = "type",
+        label = "Select a dataset",
+        choices = c("TMT", "LFQ", "Phosphosite", "mRNA", "Protein vs mRNAs")
       ),
+      conditionalPanel(
+        condition = "!(input.type == 'Protein vs mRNAs')",
+        # Dropdown to select which plot you want based on dataset
+        selectizeInput(
+          inputId = "subtype",
+          label = "Select an option",
+          choices = NULL,
+          multiple = FALSE
+        )
+      ),
+      conditionalPanel(
+        condition = "input.subtype == 'Multiplot' || input.type == 'Protein vs mRNAs'",
+        # Dropdown to select genes
+        selectizeInput(
+          inputId = "genes",
+          label = NULL,
+          choices = NULL,
+          multiple = TRUE,
+          selected = NULL
+        )
+      ),
+      conditionalPanel(
+        condition = "!(input.subtype == 'Multiplot' || input.type == 'Protein vs mRNAs')",
+        # Dropdown to select gene
+        selectizeInput(
+          inputId = "gene",
+          label = NULL,
+          choices = NULL,
+          multiple = FALSE,
+          selected = NULL
+        )
+      ),
+      conditionalPanel(
+        condition = "!(input.subtype == 'Multiplot' || input.subtype == 'Mutations' || input.type == 'Phosphosite' ||input.type == 'Protein vs mRNAs')",
+        # Checkbox to select subtype options
+        checkboxGroupInput(
+          inputId = "subtype_options",
+          label = NULL,
+          choices = NULL
+        )
+      ),
+      conditionalPanel(
+        condition = "input.subtype == 'Mutations'",
+        # Dropdown specifically for mutations
+        selectInput(
+          inputId = "mutation_status",
+          label = "Mutation Status",
+          choices = mutation_specific_genes$V1
+        )
+      )
+    ),
     mainPanel(
-      plotlyOutput("plot",height = "500px")
+      plotlyOutput("plot", height = "500px")
     )
   )
 )
