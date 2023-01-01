90ba21f5b8ce1dc728b0f3d948144df2dfc62ae7
Payton Redemer <paytonredemer@gmail.com>
2023-01-01 16:53:21 -0600

    generate available plots and gene drop downs

--- global.R
+++ global.R
@@ -12,9 +12,20 @@ source("functions.R")
 
 # database
 # loaded once per server rather than every client
-# proteomics <- dbConnect(RSQLite::SQLite(), "Proteomics.db")
 database <- dbConnect(RSQLite::SQLite(), "master_shiny_app.db")
 print("Connected to database")
 
 query <- paste0("SELECT * FROM master_database;")
 dataset <- dbGetQuery(database,query)
+
+# Generate all available plots
+available_plots <- new.env(hash=TRUE)
+available_plots[["tcga_aml"]] <- c("Multiplot","FAB", "Cyto_risk", "Fusion","Mutations")
+available_plots[["beat_aml"]] <- c("Multiplot","Fusion","Mutations")
+
+# Generate all gene drop downs
+geneList <- new.env(hash=TRUE)
+for(i in dataset$Short_hand_code) {
+  query <- geneQuery(factors = c("Gene"), table = i, unique = TRUE, sort = TRUE)
+  geneList[[i]] <- dbGetQuery(database,query)
+}
